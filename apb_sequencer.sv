d96e7008863ab84f2a49c69c8af36ddba58c4293
Pradeep Changal <47114993+PRADEEPCHANGAL@users.noreply.github.com>
2023-08-21 16:11:42 +0530

    Create apb_sequencer.sv

---
+++ apb_sequencer.sv
@@ -0,0 +1,8 @@
+class apb_sequencer extends uvm_sequencer#(apb_transaction);
+  `uvm_component_utils(apb_sequencer)
+  
+    function new(string name, uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
